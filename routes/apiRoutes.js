108d88fc30ab560abcd1c5567fed1e99f11f7001
Landon Ross <landon.w.ross@gmail.com>
2021-03-01 15:43:02 -0700

    working on server.js and HTML/api.js

---
+++ routes/apiRoutes.js
@@ -0,0 +1,22 @@
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+
+const db = require('../db/db.json');
+
+// ROUTING
+
+module.exports = (app) => {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+  app.post('./api/notes', (req, res) => {
+      db.push(req.body);
+      res.json(true);
+
+  app.get('./api/notes', (req, res) => res.json(db));
+  });
+
+};
