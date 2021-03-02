4854258dcac0d97900209ac4c3a1468ac63f09f4
Landon Ross <landon.w.ross@gmail.com>
2021-03-01 17:15:02 -0700

    updated syntax and deleted some unnecessary comments

--- public/routes/apiRoutes.js
+++ public/routes/apiRoutes.js
@@ -1,6 +1,4 @@
 // LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
 
 const db = require('../../db/db.json');
 
@@ -8,15 +6,12 @@ const db = require('../../db/db.json');
 
 module.exports = (app) => {
   // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
-  app.post('./api/notes', (req, res) => {
+
+  app.post('/api/notes', (req, res) => {
       db.push(req.body);
       res.json(true);
 
-  app.get('./api/notes', (req, res) => res.json(db));
+  app.get('/api/notes', (req, res) => res.json(db));
   });
 
 };
--- public/routes/htmlRoutes.js
+++ public/routes/htmlRoutes.js
@@ -1,5 +1,4 @@
 // DEPENDENCIES
-// We need to include the path package to get the correct file path for our html
 
 const path = require('path');
 
@@ -7,8 +6,6 @@ const path = require('path');
 
 module.exports = (app) => {
   // => HTML GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases the user is shown an HTML page of content
 
   app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
