9d283fd4555dd8ef573283177d1ccc5898e31811
Landon Ross <landon.w.ross@gmail.com>
2021-03-01 22:39:09 -0700

    updated various aspects of code with help of TA and websites

--- public/routes/apiRoutes.js
+++ public/routes/apiRoutes.js
@@ -1,6 +1,5 @@
 // LOAD DATA
 
-const db = require('../../db/db.json');
 const fs = require('fs');
 
 
@@ -8,44 +7,68 @@ const fs = require('fs');
 
 module.exports = (app) => {
 
-  let notesData = [];
+    let notes = require("../../db/db.json")
 
-  app.get("/api/notes", function(err, res) {
-    try {
-      notesData = fs.readFileSync("../../db/db.json", "utf8");
-      console.log("hello!");
-      notesData = JSON.parse(notesData);
+  app.get("/api/notes", (req, res)=>{
+    return res.json(notes)
+  })
   
-      // error handling
-    } catch (err) {
-      console.log(err);
+  app.get("/api/notes/:id", (req, res) => {
+    // console.log(req.params.id)
+    const id = req.params.id;
+    let found;
+    notes.forEach(n => {
+      if (id == n.id){
+        found = n;
+        return res.json(n)
+      }
+    })
+    return res.json(false)
+  })
+
+//Takes the note and saves it for the user in the server
+  app.post("/api/notes", (req, res) => {
+    //Give the newly added notes an ID so that we can referrence them later to delete them.
+    const newNote = req.body;
+    if (notes.length === 0){
+      newNote.id = 1
+    } else {
+      newNote.id = (notes[notes.length-1].id + 1);
     }
-    res.json(notesData);
-  });
-  
-  app.post("/api/notes", function(req, res) {
-    try {
-      notesData = fs.readFileSync("../../db/db.json", "utf8");
-      console.log(notesData);
-  
-      notesData = JSON.parse(notesData);
+    notes.push(newNote);
+    let jsonNotes = JSON.stringify(notes)
+    fs.writeFile("./db/db.json", jsonNotes, function(err) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log("Note saved!");
+    })
+    res.json(true)
+  })
 
-      req.body.id = notesData.length;
+  //Delete function for saved notes
+  //Lots of help from https://www.freecodecamp.org/news/lets-clear-up-the-confusion-around-the-slice-splice-split-methods-in-javascript-8ba3266c29ae/
+  //Also, https://www.geeksforgeeks.org/how-to-read-and-write-json-file-using-node-js/ for general info.
+  app.delete("/api/notes/:id", (req, res) => {
+    const id = req.params.id;
+    notes.forEach((n, index) => {
+      if(id == n.id){
+        //splice takes the appropriate element out of the array.
+        notes.splice(index,1)
+        //Slice copies a given part of an array and returns that copied part as a new array.
+        const notesCopy = notes.slice();
+        let jsonNotes = JSON.stringify(notesCopy)
+        //After the splice/slice we are able to take the new array and paste it back into the page for the user.
+        fs.writeFile("./db/db.json", jsonNotes, function(err) {
+          if (err) {
+            return console.log(err);
+          }
+          console.log("As you command, so it shall be.");
+        })
 
-      notesData.push(req.body); 
-      notesData = JSON.stringify(notesData);
-
-      fs.writeFile("../../db/db.json", notesData, "utf8", function(err) {
-
-        if (err) throw err;
-      });
-     
-      res.json(JSON.parse(notesData));
-  
-      // error Handling
-    } catch (err) {
-      throw err;
-    }
-  });
+      }
+    })
+    res.json(true);
+  })
 
 };
--- public/routes/htmlRoutes.js
+++ public/routes/htmlRoutes.js
@@ -7,17 +7,14 @@ const path = require('path');
 module.exports = (app) => {
   // => HTML GET Requests
 
-  app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-  });
-
   app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'));
+    res.sendFile(path.join(__dirname, '../notes.html'));
+    console.log("on the right path")
   });
 
   // If no matching route is found default to index.html
 
-  // app.get('*', (req, res) => {
-  //   res.sendFile(path.join(__dirname, '/public/index.html'));
-  // });
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../index.html'));
+   });
 };
